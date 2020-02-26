cb6993e331950ef3cecc8d90562e34fb82ddd32f
Sisi <dqgsh1055@gmail.com>
2020-02-26 12:00:27 +0900

    기본 구글 로그인

---
+++ src/main/java/com/sisi/project/oauthgoogle/web/IndexController.java
@@ -0,0 +1,29 @@
+package com.sisi.project.oauthgoogle.web;
+
+import com.sisi.project.oauthgoogle.config.auth.dto.SessionUser;
+import com.sisi.project.oauthgoogle.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+//    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model){
+//        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
+
+        return "index";
+    }
+}
